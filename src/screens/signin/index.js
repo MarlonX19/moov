11ebbc2ee333d682f3555cabd3c129a7f3e34b32
marlonx19 <marlon.englemam@gmail.com>
2020-04-24 01:30:25 -0300

    Added login screen

--- src/screens/signin/index.js
+++ src/screens/signin/index.js
@@ -1,12 +1,50 @@
-import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, TouchableOpacity, TextInput } from 'react-native';
 
 import styles from './styles';
 
 export default function SignIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+
   return (
     <View style={styles.container}>
-      <Text>Log in</Text>
+      <View style={styles.topView}>
+        <Text style={styles.topText}>Entrar</Text>
+      </View>
+      <View style={styles.inputsView}>
+        <TextInput
+          style={styles.input}
+          placeholder='Email'
+          textContentType='emailAddress'
+          placeholderTextColor='#ddd'
+          onChangeText={text => setEmail(text)}
+          value={email}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder='Senha'
+          placeholderTextColor='#ddd'
+          secureTextEntry
+          onChangeText={text => setPassword(text)}
+          value={password}
+        />
+      </View>
+      <View style={styles.forgotPass}>
+        <Text style={styles.forgotPassText}>Esqueceu senha?</Text>
+      </View>
+      <View style={styles.btnView}>
+        <TouchableOpacity
+          onPress={() => false}
+          style={styles.btn}
+        >
+          <Text style={styles.btnText}>ENTRAR</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.signUpView}>
+        <Text style={styles.signUpText}>Não tem conta? <Text style={styles.signUpText2}>Criar</Text></Text>
+      </View>
     </View>
   );
 }
