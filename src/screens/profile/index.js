50d2c5c1e75108fe469421d854fbdb8db597b1fc
marlonx19 <marlon.englemam@gmail.com>
2020-05-31 12:47:19 -0300

    change

--- src/screens/profile/index.js
+++ src/screens/profile/index.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, Image, TextInput, KeyboardAvoidingView, ScrollView } from 'react-native';
+import { View, Text, Image, TextInput, Dimensions, KeyboardAvoidingView, ScrollView } from 'react-native';
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
 
 import Header from '../../components/Header'
 import styles from './styles';
@@ -12,47 +15,47 @@ export default function profile(props) {
     <>
       <Header head='Perfil' navigation={props.navigation} handleFun={() => false} />
       <View style={styles.container}>
-          <View style={styles.topCard}>
-            <View style={styles.imageView}>
-              <Image source={require('../../assets/profile.png')} style={{ borderRadius: 50, width: 90, height: 90 }} />
-              <Text style={styles.imageText}>Mudar foto de perfil ></Text>
-            </View>
-            <View style={styles.inputs}>
-              <Text style={{ color: 'grey' }}>Nome</Text>
-              <TextInput
-                style={styles.inputsTxt}
-                placeholder='Marlon'
-                placeholderTextColor='black'
-                onChangeText={text => setName(text)}
-                value={name}
-              />
-            </View>
-            <View style={styles.inputs}>
-              <Text style={{ color: 'grey' }}>Sobrenome</Text>
-              <TextInput
-                style={styles.inputsTxt}
-                placeholder='Englemam'
-                placeholderTextColor='black'
-                onChangeText={text => setLastName(text)}
-                value={lastName}
-              />
-            </View>
+        <View style={styles.topCard}>
+          <View style={styles.imageView}>
+            <Image source={require('../../assets/profile.png')} style={{ borderRadius: 50, width: 90, height: 90 }} />
+            <Text style={styles.imageText}>Mudar foto de perfil ></Text>
           </View>
+          <View style={styles.inputs}>
+            <Text style={{ color: 'grey' }}>Nome</Text>
+            <TextInput
+              style={styles.inputsTxt}
+              placeholder='Marlon'
+              placeholderTextColor='black'
+              onChangeText={text => setName(text)}
+              value={name}
+            />
+          </View>
+          <View style={styles.inputs}>
+            <Text style={{ color: 'grey' }}>Sobrenome</Text>
+            <TextInput
+              style={styles.inputsTxt}
+              placeholder='Englemam'
+              placeholderTextColor='black'
+              onChangeText={text => setLastName(text)}
+              value={lastName}
+            />
+          </View>
+        </View>
 
-          <View style={styles.bottomCard}>
-            <View style={styles.fieldsView}>
-              <Text style={styles.phoneText}>Número de telefone</Text>
-              <Text style={styles.phoneValue}>(19) 999****51 ></Text>
-            </View>
-            <View style={styles.fieldsView}>
-              <Text style={styles.phoneText}>E-mail</Text>
-              <Text style={styles.phoneValue}>marlon.eng******@gmail.com ></Text>
-            </View>
-            <View style={styles.fieldsView}>
-              <Text style={styles.phoneText}>Senha</Text>
-              <Text style={styles.phoneValue}>Alterar ></Text>
-            </View>
+        <View style={styles.bottomCard}>
+          <View style={styles.fieldsView}>
+            <Text style={styles.phoneText}>Número de telefone</Text>
+            <Text style={styles.phoneValue}>(19) 999****51 ></Text>
           </View>
+          <View style={styles.fieldsView}>
+            <Text style={styles.phoneText}>E-mail</Text>
+            <Text style={styles.phoneValue}>marlon.eng******@gmail.com ></Text>
+          </View>
+          <View style={styles.fieldsView}>
+            <Text style={styles.phoneText}>Senha</Text>
+            <Text style={styles.phoneValue}>Alterar ></Text>
+          </View>
+        </View>
       </View>
     </>
   )
