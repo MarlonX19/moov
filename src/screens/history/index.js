8c4e2e5ee6f6a1bba629a9c7b4daa76023ef2b91
marlonx19 <marlon.englemam@gmail.com>
2020-09-06 12:57:49 -0300

    added image for no-rides

--- src/screens/history/index.js
+++ src/screens/history/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 
 import Header from '../../components/Header';
+
+import NoRide from '../../assets/no-rides.png';
+
 
 import styles from './styles';
 
 function History(props) {
   return (
-    <View>
+    <View style={styles.container}>
       <Header head='Histórico' navigation={props.navigation} />
-      <Text>history</Text>
+      <View style={styles.NoRides}>
+        <Image source={NoRide} style={{ width: 150, height: 150 }} />
+        <Text style={styles.NoRidesText}>Nada para mostrar</Text>
+      </View>
     </View>
   )
 }
--- src/screens/history/styles.js
+++ src/screens/history/styles.js
@@ -3,7 +3,19 @@ import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor:'#fff',
+    flex: 1,
+  },
 
+  NoRides: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  NoRidesText: {
+    fontSize: 20,
+    color: 'grey',
   }
 })
 
