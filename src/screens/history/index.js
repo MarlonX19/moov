bea28645ddc580de083523791809285aa81cff93
marlonx19 <marlon.englemam@gmail.com>
2020-09-06 16:10:55 -0300

    feat(History): added loading component

--- src/screens/history/index.js
+++ src/screens/history/index.js
@@ -1,14 +1,48 @@
-import React from 'react';
-import { View, Text, Image } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, Image, ActivityIndicator } from 'react-native';
 
 import Header from '../../components/Header';
 
 import NoRide from '../../assets/no-rides.png';
-
+import { api } from '../../services/auth';
 
 import styles from './styles';
 
 function History(props) {
+  const [deliveries, setDeliveries] = useState([]);
+  const [user_id, setUserId] = useState(1);
+  const [loading, setLoading] = useState(true);
+
+
+  async function fetchDeliveries() {
+    setLoading(true);
+    let type = 'users';
+    let user_id = 1;
+    const response = await api.post('/user/deliveries', { user_id, type })
+
+    if (response.data.messageCode == '200') {
+      setDeliveries(response.data.response);
+      setLoading(false);
+    } else {
+      setLoading(false);
+    }
+  }
+
+
+  useEffect(() => {
+    fetchDeliveries();
+  }, [])
+
+
+  if (loading) {
+    return (
+      <View style={styles.ActivityIndicatorView}>
+        <ActivityIndicator size='large' color='orange' />
+      </View>
+    )
+  }
+
+
   return (
     <View style={styles.container}>
       <Header head='Histórico' navigation={props.navigation} />
--- src/screens/history/styles.js
+++ src/screens/history/styles.js
@@ -3,7 +3,7 @@ import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor:'#fff',
+    backgroundColor: '#fff',
     flex: 1,
   },
 
@@ -16,6 +16,13 @@ const styles = StyleSheet.create({
   NoRidesText: {
     fontSize: 20,
     color: 'grey',
+  },
+
+  ActivityIndicatorView: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff',
   }
 })
 
