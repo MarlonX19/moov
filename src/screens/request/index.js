ca54460f7ac667b4d11c6467b45ca4ff4daf5ff6
marlonx19 <marlon.englemam@gmail.com>
2020-09-07 21:31:33 -0300

    initial components added to details screen

--- src/screens/request/index.js
+++ src/screens/request/index.js
@@ -1,5 +1,16 @@
 import React from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import Geolocation from 'react-native-geolocation-service';
+import MapViewDirections from 'react-native-maps-directions';
+import { GOOGLE_PLACES_API_KEY } from "@env";
+
+
+import finishMarker from '../../assets/finish.png';
+import startMarker from '../../assets/start.png';
+
+
+import Header from '../../components/Header';
 
 import styles from './styles';
 
@@ -11,12 +22,57 @@ function Request(props) {
   }
 
   return (
-    <View>
-      <TouchableOpacity
-        onPress={() => handleSeeDetails()}
-      >
-        <Text>ola request</Text>
-      </TouchableOpacity>
+    <View style={styles.container}>
+      <Header head='Detalhes da entrega' navigation={props.navigation} />
+      <View style={styles.mapView}>
+        <MapView
+          style={{ flex: 1 }}
+          // initialRegion={{
+          //   latitude: location.lat ? location.lat : -22.376422,
+          //   longitude: location.long ? location.long : -47.3722709,
+          //   latitudeDelta: 0.0922,
+          //   longitudeDelta: 0.0421,
+          // }}
+          showsUserLocation
+          loadingEnabled
+          showsMyLocationButton={false}
+          showsCompass={false}
+          ref={el => map = el}
+
+        >
+          <>
+            <MapViewDirections
+              origin={{"latitude": -22.5838264, "longitude": -47.40976949999999}}
+              destination={{"latitude": -23.9549113, "longitude": -46.3796944}}
+              apikey={GOOGLE_PLACES_API_KEY}
+              strokeWidth={3}
+              strokeColor='purple'
+              mode='DRIVING'
+              onReady={(result) =>
+                map.fitToCoordinates(result.coordinates)}
+            />
+            <Marker
+              title='Ponto de coleta'
+              isPreselected
+              onPress={e => console.log(e.nativeEvent)}
+              draggable
+              coordinate={{"latitude": -22.5838264, "longitude": -47.40976949999999}}
+              image={startMarker}
+            >
+            </Marker>
+            <Marker
+              title='ponto de entrega'
+              isPreselected={true}
+              coordinate={{"latitude": -23.9549113, "longitude": -46.3796944}}
+              image={finishMarker}
+            >
+            </Marker>
+          </>
+        </MapView>
+      </View>
+      <View style={styles.detailsView}>
+
+      </View>
     </View>
   )
 }
--- src/screens/request/styles.js
+++ src/screens/request/styles.js
@@ -3,7 +3,16 @@ import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+  },
+  
+  mapView:{
+    flex: 1,
+  },
 
+  detailsView:{
+    flex: 1,
+    backgroundColor: '#fff',
   }
 })
 
